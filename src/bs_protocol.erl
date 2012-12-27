4fac33da4f09258f2e580f9d80c43c0dc7c44e62
xuming <xuming@xumings-MacBook-Pro.local>
2012-12-27 15:55:20 +0800

    eee

--- src/bs_protocol.erl
+++
@@ -1,7 +0,0 @@
--module(bs_protocol).
-
--export ([parse/1]).
-
-parse(RawData) -> 
-	Message = binary_to_list(RawData),
-	{_Command,[_|_Nick]} = lists:splitwith(fun(T) -> [T] =/= ":" end ,Message).
